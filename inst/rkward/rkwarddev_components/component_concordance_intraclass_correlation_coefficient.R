cd76b28a1e7268ff186bc06c4ce8d41ac50add36
unDocUMeantIt <m@reaktanz.de>
2015-10-24 18:28:45 +0200

    changed ICC script to have R objects and JS vars in sync
      - as an example how the JS code could be better in sync with the R script objects

--- inst/rkward/rkwarddev_components/component_concordance_intraclass_correlation_coefficient.R
+++ inst/rkward/rkwarddev_components/component_concordance_intraclass_correlation_coefficient.R
@@ -3,33 +3,33 @@ rk.set.comp("Coeficiente de correlación intraclase")
 
 
 ## dialog section
-  rkt.cncintr.varselector.selector <- rk.XML.varselector(
+  selector <- rk.XML.varselector(
     label="Seleccionar variables",
     id.name="selector"
   )
 
-  rkt.cncintr.varslot.x <- rk.XML.varslot(
+  x <- rk.XML.varslot(
     label="Primera medida",
-    source=rkt.cncintr.varselector.selector,
+    source=selector,
     required=TRUE,
     types=c("factor"),
     id.name="x"
   )
 
-  rkt.cncintr.varslot.y <- rk.XML.varslot(
+  y <- rk.XML.varslot(
     label="Segunda medida",
-    source=rkt.cncintr.varselector.selector,
+    source=selector,
     required=TRUE,
     types=c("factor"),
     id.name="y"
   )
 
-  rkt.cncintr.dialog <- rk.XML.dialog(
-    rkt.cncintr.row_vars <- rk.XML.row(
-      rkt.cncintr.varselector.selector,
+  dialog <- rk.XML.dialog(
+    rowvars <- rk.XML.row(
+      selector,
       rk.XML.col(
-        rkt.cncintr.varslot.x,
-        rkt.cncintr.varslot.y
+        x,
+        y
       ),
       id.name="row_vars"
     ),
@@ -38,13 +38,13 @@ rk.set.comp("Coeficiente de correlación intraclase")
 
 
 ## wizard section
-  rkt.cncintr.wizard <- rk.XML.wizard(
+  wizard <- rk.XML.wizard(
     rk.XML.page(
       rk.XML.text(
         text="Seleccionar las variables con las medidas."
       ),
-      rkt.cncintr.copy.row_vars <- rk.XML.copy(
-        id=rkt.cncintr.row_vars
+      copy.rowvars <- rk.XML.copy(
+        id=rowvars
       )
     ),
     label="Test de correlación intraclase"
@@ -53,10 +53,10 @@ rk.set.comp("Coeficiente de correlación intraclase")
 
 ## JavaScript calculate
   rkt.cncintr.JS.calc <- rk.paste.JS(
-    x <- rk.JS.vars(rkt.cncintr.varslot.x, guess.getter=guess.getter),
-    id("var data = ", x, ".split('[[')[0];"),
-    xShort <- rk.JS.vars(rkt.cncintr.varslot.x, modifiers="shortname", guess.getter=guess.getter),
-    yShort <- rk.JS.vars(rkt.cncintr.varslot.y, modifiers="shortname", guess.getter=guess.getter),
+    xLong <- rk.JS.vars(x, guess.getter=guess.getter),
+    id("var data = ", xLong, ".split('[[')[0];"),
+    xShort <- rk.JS.vars(x, modifiers="shortname", guess.getter=guess.getter),
+    yShort <- rk.JS.vars(y, modifiers="shortname", guess.getter=guess.getter),
     rk.i18n.comment("Código R"),
     echo("results <- ICC(as.matrix(na.omit("),
     "echo(data);",
@@ -66,7 +66,7 @@ rk.set.comp("Coeficiente de correlación intraclase")
 
 ## JavaScript printout
   rkt.cncintr.JS.print <- rk.paste.JS(
-    rk.JS.header("Test de correlaci&oacute;n intraclase", addFromUI=rkt.cncintr.varslot.x, addFromUI=rkt.cncintr.varslot.y),
+    rk.JS.header("Test de correlaci&oacute;n intraclase", addFromUI=x, addFromUI=y),
     echo("rk.results (list("),
     echo(i18n("Tipo"), " = results$results$type"),
     echo(", ", i18n("ICC"), " = results$results$ICC"),
@@ -80,8 +80,8 @@ rk.set.comp("Coeficiente de correlación intraclase")
   rkt.component.intraclasscorr <- rk.plugin.component(
     about="Coeficiente de correlación intraclase",
     xml=list(
-      dialog=rkt.cncintr.dialog,
-      wizard=rkt.cncintr.wizard#,
+      dialog=dialog,
+      wizard=wizard#,
   #     snippets=,
   #     logic=
     ),
