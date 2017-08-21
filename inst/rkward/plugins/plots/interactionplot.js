60d485e731415dbb666dca2a5e5b88bbc10ae8ad
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-08-21 19:10:47 +0200

    Translation of interaction plot.

--- inst/rkward/plugins/plots/interactionplot.js
+++ inst/rkward/plugins/plots/interactionplot.js
@@ -1,48 +1,77 @@
-// author: Alfredo Sánchez Alberca (asalber@ceu.es)
-var variable, x, trace, data, variablename, xname, tracename, points, lines;
+// author : Alfredo Sánchez Alberca(asalber@ceu.es)
 
-function preprocess () {
-	echo('require(ggplot2)\n');
+include("../common/common_functions.js")
+include("../common/filter.js")
+
+var dataframe,
+  variable,
+  variableName,
+  xGroups,
+  xGroupsName,
+  trace,
+  traceName,
+  points,
+  getLines,
+  lines;
+
+function setGlobalVars() {
+  variable = getString("variable");
+  variableName = getString("variable.shortname");
+  dataframe = getDataframe(variable);
+  xGroups = getString("xGroups");
+  xGroupsName = getString("xGroups.shortname");
+  traceGroups = getString("trace");
+  traceGroupsName = getString("trace.shortname");
+  getLines = getBoolean("lines");
+}
+
+function preprocess() {
+  setGlobalVars();
+  echo('require(ggplot2)\n');
 }
 
 function calculate() {
-	variable = getString("variable");
-	x = getString("x");
-	trace = getString("trace");
-	data = variable.split('[[')[0];
-	variablename = getString("variable.shortname");
-	xname = getString("x.shortname");
-	tracename = getString("trace.shortname");
-	points = ' + geom_point(stat="summary", fun.y=mean)';
-	lines = '';
-	if (getBoolean("lines")) {
-		lines = ' + geom_line(stat="summary", fun.y=mean, aes(group=' + tracename + '))';
-	}
+  // Filter
+  filter();
+  // Means
+  points = ' + geom_point(stat="summary", fun.y=mean)';
+  // Lines
+  lines = '';
+  if (getLines) {
+    lines = ' + geom_line(stat="summary", fun.y=mean, aes(group=' + traceGroupsName + '))';
+  }
 }
 
-function printout () {
-	doPrintout (true);
+function printout() {
+  doPrintout(true);
 }
 
-function preview () {
-	preprocess();
-	calculate();
-	doPrintout (false);
+function preview() {
+  preprocess();
+  calculate();
+  doPrintout(false);
 }
 
-function doPrintout (full) {
-	if (full) {
-		echo ('rk.header ("Diagrama de interacci&oacute;n de ' + xname + ' y ' + tracename + ' sobre ' + variablename + '", list ("Variable respuesta" = rk.get.description (' + variable + '), "Grupos del eje X seg&uacute;n" = rk.get.description(' + x + '), "Grupos de trazado seg&uacute;n" = rk.get.description(' + trace + ')))\n');
-		echo ('\n');
-		echo ('rk.graph.on ()\n');
-	}
-	echo ('try ({\n');
-	echo('p<-ggplot(data=' + data + ', aes(x=' + xname + ', y=' + variablename + ', colour=' + tracename + '))' + points + lines + getString("plotoptions.code.calculate") + '\n');
-	echo('print(p)\n');	
-	echo ('})\n');
-
-	if (full) {
-		echo ('rk.graph.off ()\n');
-	}
+function doPrintout(full) {
+  // Print header
+  if (full) {
+    header = new Header(i18n("Interaction plot of %1 and %2 on %3", xGroupsName, traceGroupsName, variableName));
+    header.add(i18n("Data frame"), dataframe);
+    header.add(i18n("Response variable"), variableName);
+    header.add(i18n("x-axis grouping variable"), xGroupsName);
+    header.add(i18n("Trace grouping variable"), traceGroupsName);
+    if (filtered) {
+      header.addFromUI("condition");
+    }
+    header.print();
+    echo('rk.graph.on()\n');
+    }
+  // Plot
+  echo('try ({\n');
+  echo('interactionPlot <- ggplot(data=' + dataframe + ', aes(x=' + xGroupsName + ', y=' + variableName + ', colour=' + traceGroupsName + '))' + points + lines + getString("plotoptions.code.calculate") + '\n');
+  echo('print(interactionPlot)\n');
+  echo('})\n');
+  if (full) {
+    echo('rk.graph.off ()\n');
+  }
 }
-
