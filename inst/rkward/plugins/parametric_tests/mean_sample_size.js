8de4734c1c153f6c80a215e2ae3ae2fcdf545ced
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-08-30 20:05:31 +0200

    Translation of sample size calculation for estimating one mean.

--- inst/rkward/plugins/parametric_tests/mean_sample_size.js
+++ inst/rkward/plugins/parametric_tests/mean_sample_size.js
@@ -1,26 +1,35 @@
 // author: Alfredo Sánchez Alberca (asalber@ceu.es)
 
+include("../common/common_functions.js")
+include("../common/filter.js")
+
 // globals
-var sd, conflevel, error, errortype;
+var sd,
+  confLevel,
+  error;
 
-function preprocess () {
-    echo('require(rk.Teaching)\n');
+function setGlobalVars() {
+  sd = getString("sd");
+  confLevel = getString("confLevel");
+  error = getString("error");
 }
 
-function calculate () {
-    sd = getString("sd");
-    conflevel = getString("conflevel");
-    error = getString("error");
-    errortype = getString("error_type");	
-    echo('result <- sampleSizeOneMean(mean=, sd=' + sd + ', sig.level= 1-' + conflevel + ', error=' + error + ',error.type="' + errortype + '")\n');
+function preprocess() {
+  setGlobalVars();
+  echo('require(rk.Teaching)\n');
 }
 
-function printout () {
-    echo ('rk.header ("Sample size for estimating one mean", ');
-    echo ('parameters=list ("Standard deviation of the population" = "' + sd + '", "Confidence level" ="' + conflevel + '", "Error" = "&#177;' + error );
-    if (errortype=="relative") echo (' %');
-    echo('"))\n');
-    echo ('rk.results (list("Sample size required"= result$n))\n');
+function calculate() {
+  echo('result <- sampleSizeOneMean(mean=, sd=' + sd + ', sig.level= 1-' + confLevel + ', error=' + error + ', error.type="absolute")\n');
 }
 
-
+function printout() {
+  // Header
+  header = new Header(i18n("Sample size for estimating one mean"));
+  header.add(i18n("Standard deviation of the population"), sd);
+  header.add(i18n("Confidence level"), confLevel);
+  header.add(i18n("Error"), '&#177;' + error);
+  header.print();
+  // Sample size result
+  echo('rk.results (list(' + i18n("Sample size required") + ' = result$n))\n');
+}
