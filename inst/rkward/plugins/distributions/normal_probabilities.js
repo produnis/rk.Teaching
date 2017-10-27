1f73e6f50091f763db7cee4d81191a15dcd43028
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-10-27 20:52:07 +0200

    Translation of Simulations and some fixes.

--- inst/rkward/plugins/distributions/normal_probabilities.js
+++ inst/rkward/plugins/distributions/normal_probabilities.js
@@ -14,8 +14,12 @@ function setGlobals() {
 	plot = getBoolean("plot");
 }
 
-function calculate() {
+function preprocess() {
 	setGlobals();
+	echo('require(ggplot2)\n');
+}
+
+function calculate() {
 	echo('result <- (pnorm(q = c(' + q + '), mean = ' + mean + ', sd = ' + sd + ', ' + tail + '))\n');
 }
 
@@ -42,7 +46,7 @@ function printout() {
 		echo('y <- dnorm(x,' + mean + ',' + sd + ')\n');
 		echo('rk.graph.on()\n');
 		echo('try ({\n');
-		echo('p <- qplot(x=c(' + mean + '-3*' + sd + ',' + mean + '+3*' + sd + '), geom="blank") + geom_area(aes(x=c(x[1],x,x[100]), c(0,y,0)), fill=I("#FF9999"), alpha=0.5) + stat_function(fun=dnorm, colour="#FF5555", args=list(mean=' + mean + ', sd=' + sd + ')) + xlab(expression(italic("X"))) + ylab(expression(paste(' + i18n("Density") + '" ", italic(f(x))))) + scale_x_continuous(breaks=c(' + mean + ',' + q + '[1]))');
+		echo('p <- qplot(x=c(' + mean + '-3*' + sd + ',' + mean + '+3*' + sd + '), geom="blank") + geom_area(aes(x=c(x[1],x,x[100]), c(0,y,0)), fill=I("#FF9999"), alpha=0.5) + stat_function(fun=dnorm, colour="#FF5555", args=list(mean=' + mean + ', sd=' + sd + ')) + xlab(expression(italic("X"))) + ylab(expression(paste(' + i18n("Density") + '," ", italic(f(x))))) + scale_x_continuous(breaks=c(' + mean + ',' + q + '[1]))');
 		if (tail == "lower.tail=TRUE") {
 			echo(' + labs(title=paste("P(X<",' + q + '[1], ")=", round(result[1],4)))\n');
 		} else {
