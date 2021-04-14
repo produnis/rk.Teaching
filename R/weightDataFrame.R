3d6618252cfc9672cd17f76c4a640022181f0578
Alfredo Sánchez Alberca <asalber@gmail.com>
2021-04-14 23:13:58 +0200

    Fixed mistakes.

--- R/weightDataFrame.R
+++ R/weightDataFrame.R
@@ -2,7 +2,7 @@ weightDataFrame <- function(data, freq){
   if (ncol(data)<2) {
     stop("The data frame must contain at least two variables.")
   }
-  if (!is.numeric(data[["freq"]])) {
+  if (!is.numeric(data[[freq]])) {
     stop("The freq variable should contain frequencies and must be numeric.")
   }
   times <- as.integer(data[[freq]])
